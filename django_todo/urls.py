4cab2b90a7b6737186a3a1d4c07ac145650e461b
K-JC <kellytolfts93@hotmail.com>
2023-03-27 12:53:33 +0000

    add button to edit any todos already written

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list, add_item
+from todo.views import get_todo_list, add_item, edit_item
+from django.shortcuts import render, redirect, get_object_or_404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_todo_list, name='get_todo_list'),
     path('add', add_item, name='add'),
+    path('edit/<item_id>', edit_item, name='edit'),
 ]
